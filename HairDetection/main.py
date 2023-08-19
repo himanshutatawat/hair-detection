820a7988a1f85af1fe963073a5d990a05f1ddbf3
Himanshu Tatawat <himanshutatawat9@gmail.com>
2023-08-19 15:37:46 +0530

    Update main.py

--- HairDetection/main.py
+++ HairDetection/main.py
@@ -40,7 +40,7 @@ def train(cfg: Dict, resume: str) -> None:
 
     transforms = get_instance(module_aug, 'augmentation', cfg)
     data_loader = CustomDataLoader(transforms,
-                                   "../targetdir/sampled_images/",
+                                   "../kaggle/input/sampled-image-40/sampled_images/",
                                    20,
                                    1,
                                    .1,
